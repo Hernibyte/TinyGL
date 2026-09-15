3581a27150a60b1e661561199abdd7ec9b1aea96
Hernan <gossa.hernan@gmail.com>
2026-09-15 07:42:18 -0300

    [add] basics keycodes

---
+++ include/input_code/keyboard.h
@@ -0,0 +1,129 @@
+#pragma once
+
+// From glfw3.h
+
+/* Printable keys */
+#define TGL_KEY_SPACE              32
+#define TGL_KEY_APOSTROPHE         39  /* ' */
+#define TGL_KEY_COMMA              44  /* , */
+#define TGL_KEY_MINUS              45  /* - */
+#define TGL_KEY_PERIOD             46  /* . */
+#define TGL_KEY_SLASH              47  /* / */
+#define TGL_KEY_0                  48
+#define TGL_KEY_1                  49
+#define TGL_KEY_2                  50
+#define TGL_KEY_3                  51
+#define TGL_KEY_4                  52
+#define TGL_KEY_5                  53
+#define TGL_KEY_6                  54
+#define TGL_KEY_7                  55
+#define TGL_KEY_8                  56
+#define TGL_KEY_9                  57
+#define TGL_KEY_SEMICOLON          59  /* ; */
+#define TGL_KEY_EQUAL              61  /* = */
+#define TGL_KEY_A                  65
+#define TGL_KEY_B                  66
+#define TGL_KEY_C                  67
+#define TGL_KEY_D                  68
+#define TGL_KEY_E                  69
+#define TGL_KEY_F                  70
+#define TGL_KEY_G                  71
+#define TGL_KEY_H                  72
+#define TGL_KEY_I                  73
+#define TGL_KEY_J                  74
+#define TGL_KEY_K                  75
+#define TGL_KEY_L                  76
+#define TGL_KEY_M                  77
+#define TGL_KEY_N                  78
+#define TGL_KEY_O                  79
+#define TGL_KEY_P                  80
+#define TGL_KEY_Q                  81
+#define TGL_KEY_R                  82
+#define TGL_KEY_S                  83
+#define TGL_KEY_T                  84
+#define TGL_KEY_U                  85
+#define TGL_KEY_V                  86
+#define TGL_KEY_W                  87
+#define TGL_KEY_X                  88
+#define TGL_KEY_Y                  89
+#define TGL_KEY_Z                  90
+#define TGL_KEY_LEFT_BRACKET       91  /* [ */
+#define TGL_KEY_BACKSLASH          92  /* \ */
+#define TGL_KEY_RIGHT_BRACKET      93  /* ] */
+#define TGL_KEY_GRAVE_ACCENT       96  /* ` */
+#define TGL_KEY_WORLD_1            161 /* non-US #1 */
+#define TGL_KEY_WORLD_2            162 /* non-US #2 */
+
+/* Function keys */
+#define TGL_KEY_ESCAPE             256
+#define TGL_KEY_ENTER              257
+#define TGL_KEY_TAB                258
+#define TGL_KEY_BACKSPACE          259
+#define TGL_KEY_INSERT             260
+#define TGL_KEY_DELETE             261
+#define TGL_KEY_RIGHT              262
+#define TGL_KEY_LEFT               263
+#define TGL_KEY_DOWN               264
+#define TGL_KEY_UP                 265
+#define TGL_KEY_PAGE_UP            266
+#define TGL_KEY_PAGE_DOWN          267
+#define TGL_KEY_HOME               268
+#define TGL_KEY_END                269
+#define TGL_KEY_CAPS_LOCK          280
+#define TGL_KEY_SCROLL_LOCK        281
+#define TGL_KEY_NUM_LOCK           282
+#define TGL_KEY_PRINT_SCREEN       283
+#define TGL_KEY_PAUSE              284
+#define TGL_KEY_F1                 290
+#define TGL_KEY_F2                 291
+#define TGL_KEY_F3                 292
+#define TGL_KEY_F4                 293
+#define TGL_KEY_F5                 294
+#define TGL_KEY_F6                 295
+#define TGL_KEY_F7                 296
+#define TGL_KEY_F8                 297
+#define TGL_KEY_F9                 298
+#define TGL_KEY_F10                299
+#define TGL_KEY_F11                300
+#define TGL_KEY_F12                301
+#define TGL_KEY_F13                302
+#define TGL_KEY_F14                303
+#define TGL_KEY_F15                304
+#define TGL_KEY_F16                305
+#define TGL_KEY_F17                306
+#define TGL_KEY_F18                307
+#define TGL_KEY_F19                308
+#define TGL_KEY_F20                309
+#define TGL_KEY_F21                310
+#define TGL_KEY_F22                311
+#define TGL_KEY_F23                312
+#define TGL_KEY_F24                313
+#define TGL_KEY_F25                314
+#define TGL_KEY_KP_0               320
+#define TGL_KEY_KP_1               321
+#define TGL_KEY_KP_2               322
+#define TGL_KEY_KP_3               323
+#define TGL_KEY_KP_4               324
+#define TGL_KEY_KP_5               325
+#define TGL_KEY_KP_6               326
+#define TGL_KEY_KP_7               327
+#define TGL_KEY_KP_8               328
+#define TGL_KEY_KP_9               329
+#define TGL_KEY_KP_DECIMAL         330
+#define TGL_KEY_KP_DIVIDE          331
+#define TGL_KEY_KP_MULTIPLY        332
+#define TGL_KEY_KP_SUBTRACT        333
+#define TGL_KEY_KP_ADD             334
+#define TGL_KEY_KP_ENTER           335
+#define TGL_KEY_KP_EQUAL           336
+#define TGL_KEY_LEFT_SHIFT         340
+#define TGL_KEY_LEFT_CONTROL       341
+#define TGL_KEY_LEFT_ALT           342
+#define TGL_KEY_LEFT_SUPER         343
+#define TGL_KEY_RIGHT_SHIFT        344
+#define TGL_KEY_RIGHT_CONTROL      345
+#define TGL_KEY_RIGHT_ALT          346
+#define TGL_KEY_RIGHT_SUPER        347
+#define TGL_KEY_MENU               348
+
+#define TGL_KEY_LAST               TGL_KEY_MENU
---
+++ include/input_code/mouse.h
@@ -0,0 +1,16 @@
+#pragma once
+
+// From glfw3.h
+
+#define TGL_MOUSE_BUTTON_1         0
+#define TGL_MOUSE_BUTTON_2         1
+#define TGL_MOUSE_BUTTON_3         2
+#define TGL_MOUSE_BUTTON_4         3
+#define TGL_MOUSE_BUTTON_5         4
+#define TGL_MOUSE_BUTTON_6         5
+#define TGL_MOUSE_BUTTON_7         6
+#define TGL_MOUSE_BUTTON_8         7
+#define TGL_MOUSE_BUTTON_LAST      TGL_MOUSE_BUTTON_8
+#define TGL_MOUSE_BUTTON_LEFT      TGL_MOUSE_BUTTON_1
+#define TGL_MOUSE_BUTTON_RIGHT     TGL_MOUSE_BUTTON_2
+#define TGL_MOUSE_BUTTON_MIDDLE    TGL_MOUSE_BUTTON_3
