d3b3712eb5dbe02ac8e80d2f893adffacb1a4d6d
Hernan <gossa.hernan@gmail.com>
2026-09-22 17:15:42 -0300

    [DEL] temporal unused uniform

--- src/core/application.cpp
+++ src/core/application.cpp
@@ -19,8 +19,6 @@ namespace TGL::CORE
 
             layout (location = 0) in vec3 a_Position;
 
-            uniform mat4 transform;   
-
             void main()
             {
                 gl_Position = vec4(a_Position, 1.0);
